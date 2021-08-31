402aaf8873a309a5cae565ffcdaf108d5fb82bb9
Munnyz <65402631+Munnyz@users.noreply.github.com>
2021-08-31 16:41:22 -0300

    Add files via upload

---
+++ AgenciaBancaria.App/Program.cs
@@ -0,0 +1,38 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Endereco endereco = new Endereco("Rua test", "2223344545", "Bangu", "RJ");
+                Cliente cliente = new Cliente("Lucas", "2222", "885", endereco);
+
+                ContaCorrente conta = new ContaCorrente(cliente, 100);
+
+                Console.WriteLine("Conta " + conta.Situacao+": " + conta.NumConta + " - " + conta.DigitoVerificador);
+
+                string senha = "abc123";
+                conta.Abrir(senha);
+                Console.WriteLine("Conta " + conta.Situacao + ": " + conta.NumConta + " - " + conta.DigitoVerificador);
+
+               
+                conta.Sacar(10, senha);
+
+                Console.WriteLine("Saldo: "+ conta.Saldo);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            
+        }
+    }
+}
