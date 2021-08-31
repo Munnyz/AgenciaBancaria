41be85790d15482688d1f7a35d98bb10bc9005bb
Munnyz <65402631+Munnyz@users.noreply.github.com>
2021-08-31 16:37:00 -0300

    Add files via upload

---
+++ AgenciaBancaria.Dominio/ContaBancaria.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public abstract class ContaBancaria
+    {
+        public ContaBancaria(Cliente cliente)
+        {
+            Random random = new Random();
+            NumConta = random.Next(50000, 100000);
+            DigitoVerificador = random.Next(0, 9);
+
+            Situacao = SituacaoConta.Criada;
+
+            Cliente = cliente ?? throw new Exception("Cliente Deve ser informado");
+        }
+
+        public void Abrir(string senha)
+        {
+            SetaSenha(senha);
+            Situacao = SituacaoConta.Aberta;
+            DataAbertura = DateTime.Now;
+        }
+
+        private void SetaSenha(string senha)
+        {
+            senha = senha.ValidarStringVazia();
+            if (!Regex.IsMatch(senha, @"^(?=.*?[a-z])(?=.*?[0-9]).{5,}$"))
+            {
+                throw new Exception("Senha inválida!");
+            }
+
+            Senha = senha;
+        }
+
+        public virtual void Sacar(decimal valor, string senha)
+        {
+            if (Senha != senha)
+            {
+                throw new Exception("Senha Inválida!");
+            }
+            if (Saldo < valor)
+            {
+                throw new Exception("Saldo insuficiente!");
+            }
+            Saldo -= valor;
+        }
+
+        public int NumConta { get; init; }
+        public int DigitoVerificador { get; init; }
+        public decimal Saldo { get; protected set; }
+        public DateTime? DataAbertura { get;private set; }
+        public DateTime? DataEncerramento { get;private set; }
+        public SituacaoConta Situacao { get;private set; }
+        public string Senha { get;private set; }
+        public Cliente Cliente { get; init; }
+    }
+}
